eabc295f6e8c7d04bdb40e5cd9e75960073a1001
coder-shivam <innocentboy9472@gmail.com>
2025-06-20 22:51:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,24 +29,39 @@ def imagepro(image):
         transforms.ToTensor()
     ])
     return transform(image).unsqueeze(0)
-
-upload = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-if upload is not None:
-    image = Image.open(upload).convert('RGB')
-    images=image.resize((224,224))
-    st.image(images, caption="Uploaded Image")
-
+def labe(image):
     with st.spinner("Processing..."):
         i_tensor = imagepro(image)
         with torch.no_grad():
             outputs = model(i_tensor)
-            
             a, predicted = torch.max(outputs, 1)
             label = label[predicted.item()]
-
+    
     st.success(f"Predicted Items is: {label}")
     topic = label
+    return topic
+def gen(topic,max_len,):
+    set_seed(42)
+    result = generator(label, max_length=max_len, num_return_sequences=1)
+    st.success("Done!")
+    st.write("### Generated Text:")
+    st.write(result[0]["generated_text"])
+    
+gun=st.button("Generate text" )
+upload = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+max_len = st.slider("Max length Of Text", 50, 300, 100, step=10)
+
+if upload is not None:
+    image = Image.open(upload).convert('RGB')
+     image=image.resize((224,224))
+    if gun:
+        gen(label,max_label)
+   
+    
+    st.image(image, caption="Uploaded Image")
+     labe(image)
+
+   
     max_len = st.slider("Max length", 50, 300, 100, step=10)
 
     if st.button("Generate"):
